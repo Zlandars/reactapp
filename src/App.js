c477f4ea0b4964c63ddfe1c2785edc1c8aeb1b4c
Zland <mag20102009@gmail.com>
2022-08-14 22:45:35 +0300

    thirdLesson

--- src/App.js
+++ src/App.js
@@ -1,54 +1,126 @@
 // import logo from './logo.svg';
-import {useEffect, useRef, useState} from "react";
-import Message from "./modules/Message";
+import {
+    createTheme,
+    ThemeProvider,
+    TextField,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText, Avatar, ListItemAvatar, Typography, Divider
+} from "@mui/material";
+import React from 'react';
+import { green, orange } from "@mui/material/colors";
+import { useEffect, useRef, useState } from "react";
+import Message from "./components/Message";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+
+
+const theme = createTheme({
+    status: {
+        main: green[700],
+        danger: orange[500],
+    },
+});
 
 function App() {
-    const inputEl = useRef();
-    const formEl = useRef();
-  const [messageList, setMessageList] = useState([{
-      author: 'Evgen',
-      msg: '1'
-  },{
-      author: 'Vovke',
-      msg: 'e.target.value'
-  }]);
-  function error(params) {
-      console.log('Введите в поле данные ' + params.labels[0].innerText);
-  }
-  useEffect(()=>{
-      document.querySelector('.mainBlock').append('<div>МОЛОДЧИК</div>')
-  },[messageList])
+  const formEl = useRef(null);
+  const [messageList, setMessageList] = useState([
+    {
+      id: 1,
+      author: "Evgen",
+      msg: "1",
+    },
+    {
+      id: 2,
+      author: "Vovke",
+      msg: "e.target.value",
+    },
+  ]);
+  const [answer, setAnswer] = useState();
+  const [chatList, setChatsList] = useState([]);
+  useEffect(() => {
+    if (
+      messageList.length > 2 &&
+      messageList[messageList.length - 1] !==
+        messageList[messageList.length - 2]
+    ) {
+      setTimeout(() => {
+          setAnswer(messageList[messageList.length - 1].author);
+
+      }, 1000);
+    }
+  }, [messageList]);
   return (
-    <div className="mainBlock">
+    <ThemeProvider theme={theme}>
+      <div className="mainBlock">
+          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', flexGrow: 1 }}>
+              <ListItem alignItems="flex-start">
+                  <ListItemAvatar>
+                      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                  </ListItemAvatar>
+                  <ListItemText
+                      primary="Brunch this weekend?"
+                      secondary={
+                          <React.Fragment>
+                              <Typography
+                                  sx={{ display: 'inline' }}
+                                  component="span"
+                                  variant="body2"
+                                  color="text.primary"
+                              >
+                                  Ali Connors
+                              </Typography>
+                              {" — I'll be in your neighborhood doing errands this…"}
+                          </React.Fragment>
+                      }
+                  />
+              </ListItem>
+              <Divider variant="inset" component="li" />
 
-        <form action="" ref={formEl}>
-            <h2>Вариант 1: добавление сообщения через форму</h2>
-            <label htmlFor="author">Автор сообщения</label>
-            <input id='author' type="text" placeholder='Author'/>
-            <label htmlFor="message">Сообщение</label>
-            <input id='message' type="text" placeholder='Введите текст сообщения' />
-            <button onClick={(event)=>{
-                event.preventDefault();
-                setMessageList([
-                    ...messageList,
-                    {
-                        author: formEl.current[0].value ? formEl.current[0].value : error(formEl.current[0]),
-                        msg: formEl.current[1].value ? formEl.current[1].value : error(formEl.current[1])
-                    }
-                ]);
-                formEl.current[0].value = '';
-                formEl.current[1].value = '';
-                }
-            }>Button</button>
-        </form>
+          </List>
+          <div className="chat">
+              <Box component="form" noValidate
+                         autoComplete="off" mt={4} display="flex"
+                         flexDirection="column" onSubmit={(event) => {
+                                event.preventDefault();
+                                setMessageList([
+                                    ...messageList,
+                                    {
+                                        id: messageList.length + 1,
+                                        author: event.target.author.value,
+                                        msg: event.target.text.value,
+                                    },
+                                ]);
+                                event.target.author.value = '';
+                                event.target.text.value = '';
+                                event.target.author.focus();
+                         }}>
 
-        {messageList.map((item)=>{
-            return (
-                    <Message obj={item} />
-                );
-        })}
+                        <TextField id="name" label="Имя"
+                                   variant="outlined" sx={{ mb: 2 }}
+                                   name="author"
+                                   inputRef={formEl}
+                        />
+                        <TextField id="message" label="Сообщение"
+                                   variant="outlined" sx={{ mb: 2 }}
+                                   name="text"
+                                   inputRef={formEl}
+                        />
+                        <Button variant="outlined"
+                                size="small" type="submit" sx={{ mb: 4 }}>Отправить</Button>
+                    </Box>
 
+                      {messageList.map((item) => {
+                        return <Message obj={item} key={item.id} />;
+                      })}
+          </div>
+          {answer && <p>Спасибо за комментарий {answer}</p>}
     </div>
+    </ThemeProvider>
+    
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
